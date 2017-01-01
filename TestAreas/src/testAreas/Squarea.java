d4c507ff06d91bef523a3cb2377af3e3665d92ff
Ramon A. Chavez <rachavez1974@gmail.com>
2017-01-01 00:43:33 -0500

    Added a new class called Squarea, this class inherits from class Rectarea.

---
+++ TestAreas/src/testAreas/Squarea.java
@@ -0,0 +1,17 @@
+package testAreas;
+
+public class Squarea extends Rectarea{
+
+	public static int numOfSquares =0;
+	private double length;
+	private double width;
+	
+	public Squarea(){
+		length = 0;
+		length = 0;
+	}//Squarea constructor ends here
+	//**********************************************
+	
+	
+	
+}//Squarea class ends here
